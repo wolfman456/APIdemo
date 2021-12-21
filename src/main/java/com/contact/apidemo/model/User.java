66105213361132bca3b69d14c9bed605539da8d0
Paul Shook <pshook456@gmail.com>
2021-12-20 18:49:50 -0600

    added contact model

--- src/main/java/com/contact/apidemo/model/User.java
+++ src/main/java/com/contact/apidemo/model/User.java
@@ -18,5 +18,47 @@ public class User {
 
     @Column
     private Integer phone;
+
+    public User() {
+    }
+
+    public User(Long id, String name, String email, Integer phone) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getPhone() {
+        return phone;
+    }
+
+    public void setPhone(Integer phone) {
+        this.phone = phone;
+    }
 }
 
