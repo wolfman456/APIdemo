14ac1d07713ff90faaba2825306f7d68414f277a
Paul Shook <pshook456@gmail.com>
2022-01-26 12:03:07 -0600

    add get all method

--- src/main/java/com/contact/apidemo/service/ServiceImpl/UserServiceImpl.java
+++ src/main/java/com/contact/apidemo/service/ServiceImpl/UserServiceImpl.java
@@ -7,6 +7,8 @@ import com.contact.apidemo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class UserServiceImpl implements UserService {
     @Autowired
@@ -26,4 +28,10 @@ public class UserServiceImpl implements UserService {
         }
 
     }
+
+    @Override
+    public List<User> getAllUsers() {
+        return userRepo.findAll();
+    }
+
 }
